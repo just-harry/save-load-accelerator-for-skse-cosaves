5a37c014167e98687dfe5e84d2471447b422bf60
Harry Gillanders <contact@harrygillanders.com>
2026-08-21 00:46:35 +0100

    Update the version-number to 1.3.3.

--- mod/slack_common/user_interface.d
+++ mod/slack_common/user_interface.d
@@ -11,7 +11,7 @@ import slack_common.text;
 import slack_common.threading;
 
 
-enum immutable(wchar[]) errorDialogTitle = "Save & Load Accelerator for SKSE Cosaves v1.3.2 Error";
+enum immutable(wchar[]) errorDialogTitle = "Save & Load Accelerator for SKSE Cosaves v1.3.3 Error";
 
 
 /+ The implementation for Skyrim v1.5.97 breaks message-boxes in other versions of Skyrim.
